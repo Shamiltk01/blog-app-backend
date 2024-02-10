16ae1182156fd725944a1fa8a173bfd686648c56
Shamiltk01 <shamiltk02@gmail.com>
2024-02-10 10:31:50 +0530

    compleate

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express=require("express")
-const cors=require("cors")
 const mongoose=require("mongoose")
-const blogRouter=require("./control/blogRouter")
-
+const cors=require("cors")
+const usersRouter=require("./routers/usersRouter")
+const postRouter=require("./routers/postRouter")
 const app=express()
-
 app.use(express.json())
 app.use(cors())
+mongoose.connect("mongodb+srv://avanthik:avanthik@cluster0.yuxak7x.mongodb.net/blogDb?retryWrites=true&w=majority",{useNewUrlParser:true})
 
 
-mongoose.connect("mongodb+srv://shamiltk02:shamiltk98@cluster0.7syqm.mongodb.net/blogDb?retryWrites=true&w=majority",{useNewUrlParser:true})
+app.use("/user",usersRouter)
+app.use("/post",postRouter)
 
-app.use("/api/blog",blogRouter)
-
-app.listen(3001,()=>{
-    console.log("server running...")
+app.listen(3000,()=>{
+  console.log("server is running")
 })
-
